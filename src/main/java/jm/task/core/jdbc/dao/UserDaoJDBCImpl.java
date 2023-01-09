f1a5a67333aff0267de1f9349fbbbab088bf0633
Maloy <nikitasvetlicnyj13615@gmail.com>
2023-01-09 12:27:37 +0300

    Создал класс Util, настроил подключение к БД.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,23 +1,48 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        String sql = "CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30),"
+                + "lastName VARCHAR(30), age INT)";
+        try (Statement st = connection.createStatement()) {
+            st.executeUpdate(sql);
+            System.out.println("Таблица созд");
+        } catch (SQLException e) {
+            System.out.println("ошибка созд табл");
+        }
+
+
 
     }
 
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS user";
+        try (Statement st = connection.createStatement()) {
+            st.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("соед с бд установленно");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("НЕ соед с бд");;
+        }
+        return connection;
+    }
 }
